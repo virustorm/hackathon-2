e1dd062bf4ba1baea8257a9bee4d7b61a2d0bde5
a7jamali@gmail.com <Ali@Dominics-iPhone.localdomain>
2019-11-15 12:45:56 -0800

    styling

--- spotify/src/components/Body.js
+++ spotify/src/components/Body.js
@@ -14,7 +14,7 @@ export default class Body extends Component {
 
 	componentDidMount() {
 		axios.defaults.headers.common['Authorization'] =
-			'Bearer BQBE7D9SdXMnyiPwIKEslGn_rFP1v8997hji4NJ0NIPGLOWG7k59aP0w5nkthIIUF5C1278AQGnbUsNzWC8q9opNgL6wr5mcq-giQxkKc1bxi5GpZnIMfUT362tfGlxJ9PAbO6Zrz49_';
+			'Bearer BQA3Bcj4yplWWCrftpbXSC1AGc8J2J-5fBYLSr8c2RRpBZgZvgv2RGeLxBVZnQFaShW6LrzgEbRHUGeJgA995ZlrL1hNLWBv6WTWNompGRgrgZxMX1TL8Sh4N3K2YxGQspGfs37IcZGl';
 		axios.get(`https://api.spotify.com/v1/playlists/${this.state.id}/tracks`).then((res) => {
 			console.log(res.data.items);
 			this.setState({
@@ -25,8 +25,7 @@ export default class Body extends Component {
 
 	render() {
 		return (
-			<div>
-				<h1>popluarity range from 1 - 100</h1>
+			<div className="info__body">
 				<Info data={this.state} />
 			</div>
 		);
--- spotify/src/components/Info.js
+++ spotify/src/components/Info.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { interfaceDeclaration } from '@babel/types';
 
 export default function Info(props) {
 	console.log(props.data);
 	console.log(props.data.tracks);
-
-	//let attn = {data.track.duration_ms} / {data.track.popularity}
 
 	const info =
 		props.data.tracks &&
 		props.data.tracks.map((data, index) => {
 			let duration  = data.track.duration_ms
 			let pop = data.track.popularity
-			let attn = (duration/1000) / (pop + 1) 
+			let attn = Math.round((duration/100) / (pop + 1))
 			return (
 				<div key={index} className="info__cell">
-					<h1 className="info__main"><span className="info__track">{data.track.album.name}</span> by <span className="info__artist">{data.track.artists[0].name}</span></h1>
-					<h1 className="info__attn">Attention IDX <span className="info__number">{attn}</span></h1>
+					<h1 className="info__track">{data.track.album.name}</h1>
+					<h1 className="info__artist">{data.track.artists[0].name}</h1>
+					<h1 className="info__attn">attn.IDX <span className="info__number">{attn}</span></h1>
 				</div>
 			);
 		});
-	return <div>{info}</div>;
+	return <div className="info__box">{info}</div>;
 }
